b0ab37d2cc9312f63c52689ad947e9e9871f5e0a
poseidonchik <muha.rox@gmail.com>
2021-11-07 15:01:37 +0300

    Убрана визуализация

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -17,13 +17,13 @@ public class TestBase {
     static void setUp(){
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--disable-gpu");
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         Configuration.browserCapabilities = capabilities;
         Configuration.browserSize = "1024x768";
-        Configuration.headless = false;
+        Configuration.headless = true;
 //        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
 //        Configuration.browser = "chrome";
 //        System.setProperty("web.chrome.driver", "src/test/resources/chromedriver");
